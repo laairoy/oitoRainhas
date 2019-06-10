adef39b79f6f0cae03b8c6cd997c7d0e18188d06
Marden Laairoy <laairoy@yahoo.com.br>
2019-06-10 00:21:23 -0300

    Commit inicial de configuracao do repositorio.

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author laairoy
+ */
+public class Main {
+    private int tamanhoPopulacao;
+    private double taxaMutacao;
+    private int criterioParada;
+    
+    public static void main(String[] args){
+        
+        
+    }
+    
+}
